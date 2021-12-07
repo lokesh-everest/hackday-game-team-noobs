eee27b838f5cb2280283834206ebffb405acc327
Adharsh Rajan <adharsh.rajan@everest.engineering>
2021-12-07 13:01:48 +0530

    Add utils for shuffling cards

---
+++ src/utils/shuffle.ts
@@ -0,0 +1,10 @@
+import Card from "../models/card";
+
+export function shuffleCards(cards: Array<Card>) {
+    for (let i = cards.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = temp;
+    }
+}
