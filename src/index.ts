b8906d00e0ae522e48812900704bd35555892901
Lokesh Rongali <lokesh@everest.engineering>
2021-12-07 14:22:35 +0530

    Add websockets events for joining room and sending message

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,58 @@
-const http = require('http');
+import express from 'express'
+import {Server} from 'socket.io'
+import http from 'http'
+import cors from 'cors'
+// @ts-ignore
+import {addUser, removeUser, getUser, getUsersInRoom, User} from "./users"
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const path = require('path')
 
-const server = http.createServer((req: any, res: any) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Welcome to Uno web....');
-});
+const PORT = process.env.PORT || 3000
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const app = express()
+const index = http.createServer(app)
+const io = new Server(index)
+
+app.use(cors())
+io.on('connection', socket => {
+    socket.on('join', (payload, callback) => {
+        console.log(payload);
+        // @ts-ignore
+        const {error, newUser}: User | Error = addUser({
+            id: socket.id,
+            name: payload.name,
+            room: payload.room
+        })
+
+        if (error)
+            return callback(error)
+
+        console.log(newUser);
+        socket.join(newUser.room)
+
+        io.to(newUser.room).emit('roomData', {room: newUser.room, users: getUsersInRoom(newUser.room)})
+        socket.emit('currentUserData', {name: newUser.name})
+        callback()
+    })
+
+    socket.on('sendMessage', (payload, callback) => {
+        const user = getUser(socket.id)
+        // @ts-ignore
+        io.to(user.room).emit('message', {user: user.name, text: payload.message})
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+        if (user)
+            io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
+    })
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+index.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
---
+++ src/users.ts
@@ -0,0 +1,42 @@
+type User = {
+    id: string,
+    name: string,
+    room: string
+}
+
+type NewUser = {
+    newUser: User
+}
+
+type Error = {
+    error?: string
+}
+
+const users: User[] = []
+
+const addUser = ({id, name, room}: User): NewUser | Error => {
+    const numberOfUsersInRoom = users.filter(user => user.room === room).length
+    if (numberOfUsersInRoom === 2)
+        return {error: 'Room full'}
+
+    const newUser = {id, name, room}
+    users.push(newUser)
+    return {newUser}
+}
+
+const removeUser = (id: String) => {
+    const removeIndex = users.findIndex(user => user.id === id)
+
+    if (removeIndex !== -1)
+        return users.splice(removeIndex, 1)[0]
+}
+
+const getUser = (id: String): User | undefined => {
+    return users.find(user => user.id === id)
+}
+
+const getUsersInRoom = (room: String): User[] => {
+    return users.filter(user => user.room === room)
+}
+
+export {addUser, removeUser, getUser, getUsersInRoom, User}
