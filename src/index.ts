2168b58c6f1dd8cfe552fe7483c2fad94042bbda
Adharsh Rajan <adharsh.rajan@everest.engineering>
2021-12-07 12:07:11 +0530

    Move index.js to src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req: any, res: any) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Welcome to Uno web.');
+  res.end('Welcome to Uno web....');
 });
 
 server.listen(port, hostname, () => {
