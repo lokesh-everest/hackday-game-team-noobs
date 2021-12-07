2ee08e6751d0e3809b67110a938584359f9fdaa1
Adharsh Rajan <adharsh.rajan@everest.engineering>
2021-12-07 16:29:55 +0530

    Move enums to enums folder

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -1,4 +1,4 @@
-import { Color } from "../models/color";
+import { Color } from "../enums/color";
 
 export const cards = [
     {
--- src/models/card.ts
+++ src/models/card.ts
@@ -1,4 +1,4 @@
-import { Color } from "./color";
+import { Color } from "../enums/color";
 import { Digit } from "./types";
 
 export default class Card {
